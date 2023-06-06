92a4019b58c9e3eee43d4761684ba236af3095a5
jbarcs92 <jbarcl92@gmail.com>
2023-06-06 10:04:08 -0400

    Setup blackjack table

---
+++ main.js
@@ -0,0 +1,36 @@
+/*----- constants -----*/
+// Define an object for the different chip amounts.
+//Define an object for the cards that includes the card, and it's numerical value.
+
+
+
+  /*----- state variables -----*/
+//Define variables for whose turn it is, the winner, the player and dealers total, 
+// and the players current balance.
+
+  /*----- cached elements  -----*/
+//store the different chip amount buttons
+//store the deal, stand, hit, and new bet buttons
+//store win/no-win/push message
+//store the players hand
+//store teh dealers hand
+
+  /*----- event listeners -----*/
+//add event listener for when a betting chip is selected
+//add event listener for when selecting the deal, stand, hit, and new bet buttons
+
+  /*----- functions -----*/
+  //initialize state variables
+  //render the table
+  //render the win/no-win/push message
+  //render the deal, stand, hit, and new bet buttons
+
+  //handle player selecting a bet amount
+  //handle the player selecting the deal button
+  //handle the player selecting the hit button
+  //handle the player selecting tht stand button
+    //After player stands, flip turn to dealer by multiplying by -1
+    //Dealer will keep hitting or stand based off current total
+//compare player and dealer totals to determine message to display
+//update player balance with bet amount won/lost
+//handle the player selecting the new bet button
